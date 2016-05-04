066bb6634005fefd18742eff6b3ed78887462ba5
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-04 18:32:07 +0300

    login page is now a php

--- ActiveC/V0.1/partials/main.php
+++ ActiveC/V0.1/partials/main.php
@@ -1,4 +1,3 @@
-<?php session_start(); ?>
 <div id="main_wrap">
     <div id = "filter_main">
     Filter<br>
@@ -21,6 +20,7 @@ two
     </div>
     <div id = "std_info">
 	<?php
+		session_start();
 		$link = mysql_connect("localhost", $_SESSION["user_name"], $_SESSION["user_pass"]);
 		if (!$link) {
 			die("Could not connect: " . mysql_error());
