b12537aae7e8e78eccba15781652e72fd9fab201
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-07 20:18:45 +0300

    building mail system

--- ActiveC/V0.1/partials/mail.php
+++ ActiveC/V0.1/partials/mail.php
@@ -1,18 +1,17 @@
 
 <?php
-echo "dscdscdscdsc";
     if(isset($_POST)){
-        echo "<script>   console.log('csdcsdds');   </script>";
-    }
-   /* $to      = 'ohayon109@gmail.com';
-    $subject = 'the subject';
-    $message = 'hello';
-    $headers = 'From: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
-        'Reply-To: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
+        $to      = 'ohayon109@gmail.com';
+        $subject = 'the subject';
+        $message = 'hello';
+        $headers = 'From: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
+            'Reply-To: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
 
-    $sent_mail = mail($to, $subject, $message, $headers);
-    if ( ! $sent_mail){
-        echo "<script> alert('lala');</script>";
-    }*/
+        $sent_mail = mail($to, $subject, $message, $headers);
+        if ( ! $sent_mail){
+            echo "<script> alert('lala');</script>";
+        }
+    }
+
 ?>
