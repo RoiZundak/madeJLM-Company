c6f173e74c7131638426281e14f2ccb161deda66
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-07 20:23:32 +0300

    building mail system

--- ActiveC/V0.1/partials/mail.php
+++ ActiveC/V0.1/partials/mail.php
@@ -1,9 +1,10 @@
 
 <?php
+//contact_email
     if(isset($_POST)){
-        $to      = 'ohayon109@gmail.com';
-        $subject = 'the subject';
-        $message = 'hello';
+        $to      = $_POST['contact_email'];
+        $subject = 'Contact mail from company';
+        $message = $_POST['contact_message']."<br>Name: ".$_POST['contact_name']."<br>phone number: ".$_POST['contact_phone'];
         $headers = 'From: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
             'Reply-To: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
