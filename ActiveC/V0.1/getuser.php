89f0ced5025f1f591185b214e77c20ccbd9d6e55
Roi <roizundak@Gmail.com>
2016-05-07 17:43:01 +0300

    Revert: adding link for iteration 1

--- ActiveC/V0.1/getuser.php
+++ ActiveC/V0.1/getuser.php
@@ -76,7 +76,7 @@
 	//filter Git
 	if($func=="2"){
 		$q = intval($_GET['q']);
-		$sql="SELECT * FROM student ";//WHERE github <> '' ORDER BY first_name DESC";
+		$sql="SELECT * FROM student WHERE github <> '' ORDER BY first_name DESC";
 		$result = mysql_query ($sql);
 		if (!$result) {
 			print_r("Error);
