7de98240550d31e1889576cfd4660b3f233afd52
Roi <roizundak@Gmail.com>
2016-05-07 18:50:42 +0300

    Func 2 issues

--- ActiveC/V0.1/getuser.php
+++ ActiveC/V0.1/getuser.php
@@ -75,7 +75,7 @@
 	}
 	//filter Git
 	if($func=="2"){
-		$sql="SELECT * FROM student WHERE github<>'' '";
+		$sql="SELECT * FROM student WHERE github <>' ' '";
     		$result = mysql_query ($sql);
     		if (!$result) {
     			print_r("Error ");
