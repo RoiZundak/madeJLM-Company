d196fb4fb89e7392a69201f81c41e90a750ab3af
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-07 20:26:29 +0300

    building mail system

--- ActiveC/V0.1/partials/mail.php
+++ ActiveC/V0.1/partials/mail.php
@@ -2,13 +2,13 @@
 <?php
 //contact_email
     if(isset($_POST)){
-        $to      = $_POST['contact_email'];
+        $to      = 'Activec.madejlm@gmail.com';
         $subject = 'Contact mail from company';
         $message = $_POST['contact_message']."<br>Name: ".$_POST['contact_name']."<br>phone number: ".$_POST['contact_phone'];
         $headers = 'From: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
             'Reply-To: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
-
+        $headers .= "MIME-Version: 1.0\r\n";
         $sent_mail = mail($to, $subject, $message, $headers);
         if ( ! $sent_mail){
             echo "<script> alert('lala');</script>";
