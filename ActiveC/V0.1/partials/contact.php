7dee9e8951c827604d3fe04eb06012daa41505b7
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-09 12:53:39 +0300

    building mail system

--- ActiveC/V0.1/partials/contact.php
+++ ActiveC/V0.1/partials/contact.php
@@ -1,3 +1,24 @@
+<?php
+if(isset($_POST['submit']))
+{
+    $to      = 'Activec.madejlm@gmail.com';
+    $subject = 'Contact mail from company';
+    $message = $_POST['contact_message'].
+        "<br><br>".
+        "<br>Name: ".$_POST['contact_name'].
+        "<br>Phone Number: ".$_POST['contact_phone'].
+        "<br>Email: ".$_POST['contact_email'];
+    $headers = 'From: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
+        'Reply-To: jobmadeinjlm@server.thinksmart.co.il' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $sent_mail = mail($to, $subject, $message, $headers);
+    if ( ! $sent_mail){
+        echo "<script> alert('lala');</script>";
+    }
+}
+?>
 <!-- Page Content -->
 
 <div class="container">
@@ -11,7 +32,7 @@
                 To contact us you can either use the git system or our ActiveC e-mail.
                 Our private e-mails are provided but not for technical support.</p>
 
-            <form role="form" method="POST" action="partials/mail.php"  >
+            <form role="form" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>"  >
                 <div class="row">
                     <div class="form-group col-lg-4">
                         <label for="input1">Name</label>
