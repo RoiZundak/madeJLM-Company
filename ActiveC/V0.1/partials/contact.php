63a48752a3fc454b9c40b1bb2df15fcc48309003
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-10 15:29:41 +0300

    Indicator for mail sent try #1

--- ActiveC/V0.1/partials/contact.php
+++ ActiveC/V0.1/partials/contact.php
@@ -1,6 +1,10 @@
 <?php
     if(isset($GLOBALS['mail_err']) && $GLOBALS['mail_err'] != '' ){
         echo $GLOBALS['mail_err'];
+        echo '<script type="text/javascript">'
+        , 'alert("in");'
+        , '</script>'
+        ;
     }
 
 
