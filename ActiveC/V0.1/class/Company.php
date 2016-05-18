86c5e54e8b8ccc8b291e3bea8aa59838af2d48e2
noyBarak <noy.brk@gmail.com>
2016-05-18 12:51:44 +0300

    add user menagment classes

--- ActiveC/V0.1/class/Company.php
+++ ActiveC/V0.1/class/Company.php
@@ -3,6 +3,7 @@
 class Student extends User {
     public function __construct()
     {
+        
         parent::__construct();
         $this->config->userTableName = 'student';
         // Start object construction
