209ea38529b8c3b292a41f65826281b8bdf1e603
noyBarak <noy.brk@gmail.com>
2016-05-21 16:20:11 +0300

    working on login page

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -16,41 +16,39 @@ if(isset($_POST['action_login'])){
 }
 ?>
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
-<div class="content">
-    <h2>Log In</h2>
-    <?php
-    if(isset($msg)){
-        echo "<h2>{$msg[0]}</h2><p>{$msg[1]}</p>";
-    }
-    ?>
-    <form action="login.php" method="POST" style="margin:0px auto;display:table;">
-        <label>
-            <p>Username / E-Mail</p>
-            <input name="login" type="text"/>
-        </label><br/>
-        <label>
-            <p>Password</p>
-            <input name="password" type="password"/>
-        </label><br/>
-        <label>
-            <p>
-                <input type="checkbox" name="remember_me"/> Remember Me
-            </p>
-        </label>
-        <div clear></div>
-        <button style="width:150px;" name="action_login">Log In</button>
-    </form>
-    <style>
-        input[type=text], input[type=password]{
-            width: 230px;
+<div id ="login_container">
+
+    <div id="login_form">
+        <br>
+        <h3>Login</h3>
+        <?php
+        if(isset($msg)){
+            echo "<script>console.log({$msg[0]} {$msg[1]})</script>";
         }
-    </style>
-    <p>
-    <p>Don't have an account ?</p>
-    <a class="button" href="register.php">Register</a>
-    </p>
-    <p>
-    <p>Forgot Your Password ?</p>
-    <a class="button" href="reset.php">Reset Password</a>
-    </p>
+        ?>
+        <form class="login"  action="login.php" method="POST">
+            <p>
+                <label for="login">Email: </label>
+                <input type="text" name="user_name" id="login"   value="Example@example.com" onfocus="if($(this).val()=='Example@example.com')$(this).val('')" onblur="if($(this).val()=='')$(this).val('Example@example.com')">
+            </p>
+
+            <p>
+                <label for="password">Password: </label>
+                <input type="password" name="user_password" id="password" value="688822292" onfocus="if($(this).val()=='688822292')$(this).val('')" onblur="if($(this).val()=='')$(this).val('688822292')">
+            </p>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="remember_me"> Remember me
+                </label>
+            </div>
+
+            <p class="login-submit">
+                <button type='submit' class="login-button" name="action_login">Log In</button>
+            </p>
+
+            <p class="forgot-password"><a href="#/forgot">Forgot your password?</a></p>
+        </form>
+    </div>
+
 </div>
