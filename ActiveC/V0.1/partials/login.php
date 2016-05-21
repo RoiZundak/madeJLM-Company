ebe51365426326473d395670ae8601c3b71262c4
noyBarak <noy.brk@gmail.com>
2016-05-21 16:11:11 +0300

    working on login page

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -1,54 +1,55 @@
 <?php
-require "../plugin/config.php";
-if(isset($_POST['action_login'])){
-    $identification = $_POST['user_name'];
-    $password = $_POST['user_password'];
-    if($identification == "" || $password == ""){
-        $msg = array("Error", "Username / Password Wrong !");
-    }else{
-        $login = \Fr\LS::login($identification, $password, isset($_POST['remember_me']));
-        if($login === false){
+require "config.php";
+\Fr\LS::init();
+?>
+<html>
+<head>
+    <title>Log In With Two Step Verification</title>
+</head>
+<body>
+<div class="content">
+    <h2>Two Step Log In</h2>
+    <p>This demo shows how logSys can be used to implement two step verification.</p>
+    <?php
+    $two_step_login_active = false;
+    if(\Fr\LS::twoStepLogin() === false && isset($_POST['action_login'])){
+        $identification = $_POST['login'];
+        $password = $_POST['password'];
+        if($identification == "" || $password == ""){
             $msg = array("Error", "Username / Password Wrong !");
-        }else if(is_array($login) && $login['status'] == "blocked") {
-            $msg = array("Error", "Too many login attempts. You can attempt login after " . $login['minutes'] . " minutes (" . $login['seconds'] . " seconds)");
+        }else{
+            $login = \Fr\LS::twoStepLogin($identification, $password, isset($_POST['remember_me']));
+            if($login === false){
+                echo "<h2>Error</h2><p>Username / Password Wrong !</p>";
+            }else if(is_array($login) && $login['status'] == "blocked"){
+                echo "<h2>Error</h2><p>Too many login attempts. You can attempt login after ". $login['minutes'] ." minutes (". $login['seconds'] ." seconds)</p>";
+            }else{
+                $two_step_login_active = true;
+            }
         }
     }
-}
-?>
-<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
-    <div id ="login_container">
-
-        <div id="login_form">
-            <br>
-            <h3>Login</h3>
-            <?php
-                if(isset($msg)){
-                   echo "<script>console.log({$msg[0]} {$msg[1]})</script>";
-                }
-            ?>
-            <form class="login"  action="login.php" method="POST">
+    if(!$two_step_login_active){
+        ?>
+        <form action="login.php" method="POST" style="margin:0px auto;display:table;">
+            <label>
+                <p>Username / E-Mail</p>
+                <input name="login" type="text"/>
+            </label><br/>
+            <label>
+                <p>Password</p>
+                <input name="password" type="password"/>
+            </label><br/>
+            <label>
                 <p>
-                    <label for="login">Email: </label>
-                    <input type="text" name="user_name" id="login"   value="Example@example.com" onfocus="if($(this).val()=='Example@example.com')$(this).val('')" onblur="if($(this).val()=='')$(this).val('Example@example.com')">
+                    <input type="checkbox" name="remember_me"/> Remember Me
                 </p>
-
-                <p>
-                    <label for="password">Password: </label>
-                    <input type="password" name="user_password" id="password" value="688822292" onfocus="if($(this).val()=='688822292')$(this).val('')" onblur="if($(this).val()=='')$(this).val('688822292')">
-                </p>
-
-                <div class="checkbox">
-                       <label>
-                        <input type="checkbox" name="remember_me"> Remember me
-                    </label>
-                </div>
-
-                <p class="login-submit">
-                    <button type='submit' class="login-button" name="action_login">Log In</button>
-                </p>
-
-                <p class="forgot-password"><a href="#/forgot">Forgot your password?</a></p>
-            </form>
-        </div>
-
-    </div>
+            </label>
+            <div clear></div>
+            <button style="width:150px;" name="action_login">Log In</button>
+        </form>
+        <?php
+    }
+    ?>
+</div>
+</body>
+</html>
