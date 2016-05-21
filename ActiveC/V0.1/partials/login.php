54a90cc7e7f7d5dbd642faa25eeedfc3a41de121
noyBarak <noy.brk@gmail.com>
2016-05-21 17:54:42 +0300

    working on login page

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -2,3 +2,50 @@
 echo("User Name: " . $_POST['username'] . "<br />\n");
 echo("Password: " . $_POST['password'] . "<br />\n");
 ?>
+
+
+<?php
+session_start();
+//DB configuration Constants
+define('_HOST_NAME_', 'localhost');
+define('_USER_NAME_', 'jobmadeinjlm');
+define('_DB_PASSWORD', 'q1w2e3r4');
+define('_DATABASE_NAME_', 'jobmadei_db');
+
+//PDO Database Connection
+try {
+    $databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
+    $databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+
+if(!empty($_POST['username'])){
+    $errMsg = '';
+    echo"111";
+    //username and password sent from Form
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+    if($username == '')
+        $errMsg .= 'You must enter your Username<br>';
+
+    if($password == '')
+        $errMsg .= 'You must enter your Password<br>';
+
+    echo "dsv";
+    if($errMsg == ''){
+        $records = $databaseConnection->prepare('SELECT id,username,password FROM  company WHERE username = :username');
+        $records->bindParam(':username', $username);
+        $records->execute();
+        $results = $records->fetch(PDO::FETCH_ASSOC);
+        if(count($results) > 0 && password_verify($password, $results['password'])){
+            $_SESSION['username'] = $results['username'];
+            header('location: main.php');
+            exit;
+        }else{
+            $errMsg .= 'Username and Password are not found<br>';
+        }
+    }
+}
+
+?>
