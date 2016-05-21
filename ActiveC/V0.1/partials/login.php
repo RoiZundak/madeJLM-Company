b10b5019e67d0a36532e6de1201b28ed2bb3f5b5
noyBarak <noy.brk@gmail.com>
2016-05-21 16:16:54 +0300

    working on login page

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -15,6 +15,7 @@ if(isset($_POST['action_login'])){
     }
 }
 ?>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
 <div class="content">
     <h2>Log In</h2>
     <?php
