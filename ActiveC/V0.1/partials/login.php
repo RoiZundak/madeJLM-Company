aef83db462d61410227e0975e0e105ecfa8133a6
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-21 17:50:40 +0300

    login page

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -1,4 +1,4 @@
 <?php
-echo("User Name: " . $_POST['user_name'] . "<br />\n");
-echo("Password: " . $_POST['user_password'] . "<br />\n");
+echo("User Name: " . $_POST['username'] . "<br />\n");
+echo("Password: " . $_POST['password'] . "<br />\n");
 ?>
