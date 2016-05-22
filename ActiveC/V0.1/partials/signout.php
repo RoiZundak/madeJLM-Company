0bf438a3da2198525b2f70924cf2c119955b4a64
noyBarak <noy.brk@gmail.com>
2016-05-22 09:55:55 +0300

    working on logout page

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -1,14 +1,8 @@
 <?php
-/**
- * User: David
- * Date: 21/05/2016
- * Time: 18:58
- */
 session_start();
-unset($_SESSION['username']);
-
+session_destroy();
+//unset($_SESSION['username']);
 header("location: ../#/login");
 exit;
-
 ?>
 
