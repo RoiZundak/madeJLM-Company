d1165865bae59db8e1cf02c00131ce363d2eef4d
yuval <yuval66ts@gmail.com>
2016-05-22 13:17:19 +0300

    jj

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -18,7 +18,7 @@ if(!empty($_POST['username'])){
     //username and password sent from Form
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
-   // echo ("<script> alert('$password');</script>");
+    echo ("<script> alert('$password');</script>");
 
     if($username == '')
         $errMsg .= 'You must enter your Username<br>';
