3220f471b99fe24c6981881fd3f8da00ad337f3c
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-22 23:29:04 +0300

    Building basic admin page

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -35,7 +35,7 @@ if(!empty($_POST['username'])){
         $records->execute();
         $results = $records->fetch(PDO::FETCH_ASSOC);
         //if(count($results) > 0 && password_verify($password, $results['password'])){
-        if(count($results) > 0 && $password === md5($results['password']) ){
+        if(count($results) > 0 && $password === $results['password'] ){
             $_SESSION['username'] = $results['username'];
             header('location: ../#/main');
             exit;
