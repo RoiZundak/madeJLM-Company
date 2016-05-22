c0b93fd6a81bda77dc44322a01d338d63fd5467d
noyBarak <noy.brk@gmail.com>
2016-05-22 12:45:36 +0300

    working on hashing password

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -24,6 +24,9 @@ if(!empty($_POST['username'])){
     if($password == '')
         $errMsg .= 'You must enter your Password<br>';
 
+   // $hash = hash('sha256', $salt1.$password.$salt2);
+
+
     if($errMsg == ''){
         $records = $databaseConnection->prepare('SELECT id,username,password FROM  company WHERE username = :username');
         $records->bindParam(':username', $username);
