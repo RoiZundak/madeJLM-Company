4a87946f5f403f2b1a85752f46ee753b9d36d26d
Moria Tuchband <moriyatu@gmail.com>
2016-05-24 13:52:46 +0300

    sign out #4

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -2,8 +2,12 @@
     session_name('complogin');
     session_start();
     //unset($_SESSION['username']);
-    session_unset($_SESSION['complogin']);     // unset $_SESSION variable for the run-time
-    session_destroy($_SESSION['complogin']);   // destroy session data in storage
+    //session_unset($_SESSION['complogin']);     // unset $_SESSION variable for the run-time
+
+    unset($_SESSION['complogin']);     // unset $_SESSION variable for the run-time
+    $_SESSION['complogin'] = "false";
+    session_destroy($_SESSION['complogin']); // destroy session data in storage
+
     echo("<a id='re_route' href ='#/login'>
         <script>
              document.getElementById(\"re_route\").click();
