eda974a7f47dff8fb619d18d4a85518f0c406253
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-24 16:56:36 +0300

    signout

--- ActiveC/V0.1/partials/signout1.php
+++ ActiveC/V0.1/partials/signout1.php
@@ -1,6 +1,6 @@
 <?php
 
 session_start(); // initialize the session variables
-$_SESSION['login_user']="3";
+$_SESSION['login_user'] = "jhgjnbvvhhjkjhgvcxcvhjkjhgfghjkjhgfghjk";
 echo "Done , move on";
 ?>
