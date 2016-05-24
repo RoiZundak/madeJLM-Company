2f4d3d2dd01234da56df7652e08fdf034a6dc29b
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-24 15:47:16 +0300

    signout

--- ActiveC/V0.1/partials/signout1.php
+++ ActiveC/V0.1/partials/signout1.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
+echo'test is '. $_SESSION['login_user']."<br>";
 session_unset();
-echo'test is '. $_SESSION['login_user'];
+echo'test is '. $_SESSION['login_user']."<br>";
 session_destroy();
-
+echo'test is '. $_SESSION['login_user']."<br>";
 
 ?>
