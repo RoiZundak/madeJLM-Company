bbcf59db58e11af4da5fd97e31c0d72bd21554f0
noyBarak <noy.brk@gmail.com>
2016-05-24 18:17:28 +0300

    working on signout page

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -3,7 +3,7 @@
     session_start();
     unset($_SESSION['username']);
     session_destroy();
-    echo("<a id='re_route' href ='../#/login'>
+    echo("<a id='re_route' href ='#/login'>
         <script>
              document.getElementById(\"re_route\").click();
                         alert('successfully logout.');
