b129a07994725f979ccbf0097512834ebd4a260d
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-24 17:34:05 +0300

    signout

--- ActiveC/V0.1/partials/signout1.php
+++ ActiveC/V0.1/partials/signout1.php
@@ -1,8 +1,10 @@
 <?php
-session_save_path("../js");
-session_start(); // initialize the session variables
-echo session_save_path()."<br>";
+    session_save_path("../js");
+    session_start(); // initialize the session variables
+    echo session_save_path()."<br>";
+    print_r($_SESSION);
+    $_SESSION=array();
 print_r($_SESSION);
-$_SESSION['login_user'] = "jhgjnbvvhhjkjhgvcxcvhjkjhgfghjkjhgfghjk";
-echo "Done , move on";
+    $_SESSION['login_user'] = "jhgjnbvvhhjkjhgvcxcvhjkjhgfghjkjhgfghjk";
+    echo "Done , move on";
 ?>
