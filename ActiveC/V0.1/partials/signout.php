9c7f26e348c88d5162afa2e24755ab2c7e66fc85
noyBarak <noy.brk@gmail.com>
2016-05-24 19:08:15 +0300

    working on blockin main while signout

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -1,12 +1,13 @@
 <?php
     session_start();
     unset($_SESSION['username']);
-    $loginName = $_SESSION['username'];
+    //$loginName = $_SESSION['username'];
     session_destroy();
     echo("<a id='re_route' href ='#/login'>
         <script>
              document.getElementById(\"re_route\").click();
-                        alert('successfully logout.--".$loginName."--');
+                        //alert('successfully logout.--".$loginName."--');
+                        alert('successfully logout.');
          </script>
      </a>");
 ?>
