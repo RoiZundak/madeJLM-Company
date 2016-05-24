80a2a40644e4b92c833f3f2735cf2b1162328487
noyBarak <noy.brk@gmail.com>
2016-05-24 18:31:29 +0300

    working on blockin main while signout

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -6,7 +6,7 @@
     echo("<a id='re_route' href ='../#/login'>
         <script>
              document.getElementById(\"re_route\").click();
-                        alert('successfully logout.--'+$loginName+'--');
+                        alert('successfully logout.--".$loginName."--');
          </script>
      </a>");
 ?>
