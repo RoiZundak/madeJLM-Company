b3e7c1bcb5d878f858324f6f11c17ad84b9e30ec
Moria Tuchband <moriyatu@gmail.com>
2016-05-24 16:11:31 +0300

    sign out with David Ohayon

--- ActiveC/V0.1/partials/signout1.php
+++ ActiveC/V0.1/partials/signout1.php
@@ -1,23 +1,8 @@
 <?php
 
-
-
-
 session_start(); // initialize the session variables
-//echo'test is '. $_SESSION['login_user'];
-session_unset(); // clear the $_SESSION variable
-
-if(isset($_COOKIE[session_name()])) {
-    setcookie(session_name(),'',time()-3600); # Unset the session id
-}
-
-session_regenerate_id();
-
-
-
-//echo'test is '. $_SESSION['login_user'];
-
-
-
+setcookie (session_id(), "", time() - 3600);
+session_destroy();
+session_write_close();
 
 ?>
