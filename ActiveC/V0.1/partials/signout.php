b2eb1c53342f818a5c07032c2bf6b528b307f468
Moria Tuchband <moriyatu@gmail.com>
2016-05-24 17:38:36 +0300

    sign out with David Ohayon

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -1,9 +1,11 @@
 <?php
 
-    session_start();
-    unset($_SESSION['loggedin']);
-    $_SESSION['loggedin'] = 0;
-    session_write_close();
+    //session_start();
+   // unset($_SESSION['loggedin']);
+   // $_SESSION['loggedin'] = 0;
+    //session_write_close();
+session_destroy();
+session_start();
 echo("<a id='re_route' href ='#/login'>
         <script>
              document.getElementById(\"re_route\").click();
@@ -11,4 +13,5 @@ echo("<a id='re_route' href ='#/login'>
              console.log('from signout : ".$_SESSION['username']."');
          </script>
      </a>");
+
 ?>
