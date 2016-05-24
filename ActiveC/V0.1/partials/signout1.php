07884b3507e420840d77d8d2029cf4574db71f49
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-24 16:48:41 +0300

    signout

--- ActiveC/V0.1/partials/signout1.php
+++ ActiveC/V0.1/partials/signout1.php
@@ -1,7 +1,9 @@
 <?php
 
 session_start(); // initialize the session variables
+$_SESSION['login_user']="3";
 session_unset();
+$_SESSION['login_user']="1";
 session_destroy();
 echo "Done , move on";
 ?>
