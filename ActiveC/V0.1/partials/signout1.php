d1f08853cf0de5c04bb84287d77d856a1571f55c
Moria Tuchband <moriyatu@gmail.com>
2016-05-24 17:42:49 +0300

    sign out with David Ohayon

--- ActiveC/V0.1/partials/signout1.php
+++ ActiveC/V0.1/partials/signout1.php
@@ -1,12 +1,11 @@
 <?php
-    session_save_path("../js");
-    session_start(); // initialize the session variables
-    echo session_save_path()."<br>";
-    print_r($_SESSION);
-    $_SESSION=array();
-print_r($_SESSION);
-    $_SESSION['login_user'] = "jhgjnbvvhhjkjhgvcxcvhjkjhgfghjkjhgfghjk";
+    //session_save_path("../js");
+   // session_start(); // initialize the session variables
+   // echo session_save_path()."<br>";
+   // print_r($_SESSION);
+   // $_SESSION=array();
+//print_r($_SESSION);
+   // $_SESSION['login_user'] = "jhgjnbvvhhjkjhgvcxcvhjkjhgfghjkjhgfghjk";
 session_destroy();
 session_start();
     echo "Done , move on";
-?>
