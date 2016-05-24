3a84228758b5824fd3a009ef562c3824d7512ff3
Moria Tuchband <moriyatu@gmail.com>
2016-05-24 16:15:53 +0300

    sign out with David Ohayon

--- ActiveC/V0.1/partials/signout1.php
+++ ActiveC/V0.1/partials/signout1.php
@@ -1,7 +1,12 @@
 <?php
 
 session_start(); // initialize the session variables
-setcookie (session_id(), "", time() - 3600);
+
+
+
+$params = session_get_cookie_params();
+setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
+
 session_destroy();
 session_write_close();
 
