122471ce26f05991663f18f15031ea4948481c26
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-26 23:10:36 +0300

    db_connect is now in a single file

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -1,22 +1,11 @@
 
 
 <?php
-	//session_start();
+	require_once "php/db_connect.php";
+	$databaseConnection =connect_to_db();
+
+
 	$func = intval($_GET['func']);
-
-//PDO connection :
-define('_HOST_NAME_', 'localhost');
-define('_USER_NAME_', 'jobmadeinjlm');
-define('_DB_PASSWORD', 'q1w2e3r4');
-define('_DATABASE_NAME_', 'jobmadei_db');
-//PDO Database Connection
-try {
-	$databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
-	$databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-	echo 'ERROR: ' . $e->getMessage();
-}
-
 
 /*mysql connection
 	$con = mysql_connect("5.100.253.198", "jobmadeinjlm","q1w2e3r4");
---
+++ ActiveC/V0.1/php/db_connect.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David
+ * Date: 26/05/2016
+ * Time: 23:08
+ */
+
+function connect_to_db(){
+    //PDO connection :
+    define('_HOST_NAME_', 'localhost');
+    define('_USER_NAME_', 'jobmadeinjlm');
+    define('_DB_PASSWORD', 'q1w2e3r4');
+    define('_DATABASE_NAME_', 'jobmadei_db');
+        //PDO Database Connection
+    try {
+        $databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
+        $databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $databaseConnection;
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+}
