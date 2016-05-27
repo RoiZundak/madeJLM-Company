c80441f2c3d5f3a003bcd55586e07b6dfa73e800
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-27 13:45:32 +0300

    connect to db in login page using db_connect.php

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -2,17 +2,9 @@
 session_start();
 //DB configuration Constants
 
-define('_HOST_NAME_', 'localhost');
-define('_USER_NAME_', 'jobmadeinjlm');
-define('_DB_PASSWORD', 'q1w2e3r4');
-define('_DATABASE_NAME_', 'jobmadei_db');
-//PDO Database Connection
-try {
-    $databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
-    $databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
+require_once "php/db_connect.php";
+$databaseConnection =connect_to_db();
+
 if(!empty($_POST['username'])){
     $errMsg = '';
 
