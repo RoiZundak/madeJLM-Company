4faf604d89524b574845581f35dd4bd806021509
Roi <roizundak@Gmail.com>
2016-05-27 15:21:41 +0300

    Signout.php

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -5,8 +5,8 @@
         session_destroy();
         echo("<a id='re_route' href ='#/login'>
         <script>
-             document.getElementById(\"re_route\").click();
-                 alert('successfully logout');
+             /*document.getElementById(\"re_route\").click();*/
+             alert('successfully logout');
          </script>
      </a>");
     }
