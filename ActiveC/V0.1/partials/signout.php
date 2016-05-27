d0449ea54e328548cd6efba72c9df78e830a92f5
Roi <roizundak@Gmail.com>
2016-05-27 15:20:34 +0300

    Signout.php

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -6,11 +6,8 @@
         echo("<a id='re_route' href ='#/login'>
         <script>
              document.getElementById(\"re_route\").click();
-                        //alert('successfully logout.--".$loginName."--');
-                        alert('successfully logout.');
+                 alert('successfully logout');
          </script>
      </a>");
     }
-    //$loginName = $_SESSION['username'];
-
-?>
+ ?>
