b5ae993d5053501a93008880ed06851ef72a3c8d
Roi <roizundak@Gmail.com>
2016-05-27 15:13:13 +0300

    Signout.php

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -1,13 +1,16 @@
 <?php
     session_start();
-    unset($_SESSION['username']);
-    //$loginName = $_SESSION['username'];
-    session_destroy();
-    echo("<a id='re_route' href ='#/login'>
+    if(!empty($_SESSION['username'])){
+         unset($_SESSION['username']);
+        session_destroy();
+        echo("<a id='re_route' href ='#/login'>
         <script>
              document.getElementById(\"re_route\").click();
                         //alert('successfully logout.--".$loginName."--');
                         alert('successfully logout.');
          </script>
      </a>");
+    }
+    //$loginName = $_SESSION['username'];
+
 ?>
