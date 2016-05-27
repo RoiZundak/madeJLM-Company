3c2112777c3abefd1797acadf232ca80a80861c1
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-27 14:46:44 +0300

    block "main" page

--- ActiveC/V0.1/partials/main.php
+++ ActiveC/V0.1/partials/main.php
@@ -3,19 +3,22 @@
     //echo($_SESSION['username']);
     if(empty($_SESSION['username']))
     {
-        echo ("
-                <script>
-                
-                    if(!document.getElementById(\"re_route\")){
-                        document.getElementById('body').innerHTML+=
-                         '  <a id=\"re_route\" href =\"./#/login\">CLICK TO GO BACK</a>   ';
-                    }
-                    alert('you MUST login first. redirecting...');
-                    /*document.getElementById(\"re_route\").click();
-                </script>
-                ");
+        echo ("<a id='re_route' href ='./#/login\'>CLICK TO GO BACK</a>");
         exit;
     }
+/*
+ * 
+ * 
+ *  <script>
+                
+                    if(!document.getElementById("re_route")){
+                        document.getElementById('body').innerHTML+=
+                         '     ';
+                    }
+                    alert('you MUST login first. redirecting...');
+                    /*document.getElementById("re_route").click();
+                </script>
+ */
 ?>
 
 <div id="main_wrap">
