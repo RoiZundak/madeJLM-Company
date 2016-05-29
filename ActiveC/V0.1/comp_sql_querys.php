10062b6ec62113fe8f4589f4c4a55d24206ff283
Roi <roizundak@Gmail.com>
2016-05-29 16:01:15 +0300

    comp_sql_querys fix hide mail

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -40,7 +40,7 @@
                 <td class ='line_td'>
                     <p>
                         <h2>" . $row['first_name'] ." ". $row['last_name'] . "</h2>"
-				."<br id='mailButton'>" .$row['Email'] . "
+				."<br id='headMail'>" .$row['Email'] . "
                     </p>
                 </td>
             </tr>
