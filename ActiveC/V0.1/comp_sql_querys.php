a66ade04898a6dc3a47de3f6d327edd83f5a3c69
yuval <yuval66ts@gmail.com>
2016-05-30 01:50:16 +0300

    start skills

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -279,10 +279,37 @@ if($func=="9") {
 
 	//PDO STYLE :
 	$sql = 'SELECT * FROM skills';
-	$img_src = "../img/profilepic.png";
+	echo "<div id=\'myModal\' class=\'modal\'><div class=\'modal-content\'><span class=\'close\'>×</span> <p>Some text in the Modal..</p>
+	</div></div>
+	<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById(\"myBtn\");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName(\"close\")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = \"block\";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = \"none\";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = \"none\";
+    }
+}
+</script>";
 	foreach ($databaseConnection->query($sql) as $row) {
 			$img_src = "../../../MadeinJLM-students/mockup/" . $row['name'];
-		echo "<div class=\'modal-content\'><span class=\'close\'>×</span> <p>Some text in the Modal..</p></div>";
 		print_r($row['name']);
 		echo "</div>";
 	}
