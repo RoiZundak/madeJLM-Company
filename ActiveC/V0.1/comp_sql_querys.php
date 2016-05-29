1210bb28fd669b60c978ed43c19574f03e045332
yuval <yuval66ts@gmail.com>
2016-05-30 02:00:40 +0300

    start skills

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -279,35 +279,35 @@ if($func=="9") {
 
 	//PDO STYLE :
 	$sql = 'SELECT * FROM skills';
-?>
+echo "
 	<script>
 		// Get the modal
 		var modal = document.getElementById('myModal');
 
 		// Get the button that opens the modal
-		var btn = document.getElementById("myBtn");
+		var btn = document.getElementById('myBtn');
 
 		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
+		var span = document.getElementsByClassName('close')[0];
 
 		// When the user clicks the button, open the modal
 		btn.onclick = function() {
-			modal.style.display = "block";
+			modal.style.display = 'block';
 		}
 
 		// When the user clicks on <span> (x), close the modal
 		span.onclick = function() {
-			modal.style.display = "none";
+			modal.style.display = 'none';
 		}
 
 		// When the user clicks anywhere outside of the modal, close it
 		window.onclick = function(event) {
 			if (event.target == modal) {
-				modal.style.display = "none";
+				modal.style.display = 'none';
 			}
 		}
 
-	</script><?php
+	</script>"
 	echo "<div id=\'myModal\' class=\'modal\'><div class=\'modal-content\'><span class=\'close\'>×</span> <p>Some text in the Modal..</p>
 	</div></div>";
 	foreach ($databaseConnection->query($sql) as $row) {
