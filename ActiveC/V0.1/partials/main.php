c5817ec22fc654e7c293ae772aebf4fe738f518c
Roi <roizundak@Gmail.com>
2016-05-30 02:20:42 +0300

    RED:main PHP work on Go back main

--- ActiveC/V0.1/partials/main.php
+++ ActiveC/V0.1/partials/main.php
@@ -7,7 +7,7 @@ echo "
             if ( name === null || name === 'Not_Valid_User_Name' ) {
                 //document.getElementById(\"re_route_login\").click();
                 document.write('<a href=\'./#/login\' id =\'link_back\' > go back</a>');
-                window.stop();
+                //window.stop();
                 <!--
             }
             </script>";
