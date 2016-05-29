5e52cbca50dd4e6624cf0a8a1ea83afbf7e1d7bf
yuval <yuval66ts@gmail.com>
2016-05-30 00:47:15 +0300

    start skills

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -156,23 +156,7 @@
 				print_r($row['first_name']);
 				echo "</div>";
 			}
-			if($func=="5"){
 
-				//PDO STYLE :
-				$sql = "SELECT * FROM student WHERE github<>''";
-				$img_src = "../img/profilepic.png";
-				foreach ($databaseConnection->query($sql) as $row) {
-					$img_src ="";
-					if(  $row['profile']=="" ){
-						$img_src = "./img/profilepic.png";
-					}else{
-						$img_src="../../../MadeinJLM-students/mockup/".$row['profile'];
-					}
-					echo "<div class='head' id='head_".$row['ID']."' > ";
-					echo "<img class='head_image' id='headimage_".$row['ID']. "' src='".$img_src."' width='120px' height='110px'>";
-					print_r($row['first_name']);
-					echo "</div>";
-				}
 
 
 		/* MYSQL APPROACH
@@ -199,6 +183,25 @@
 		*/
 			
 	}
+if($func=="5") {
+
+	//PDO STYLE :
+	$sql = "SELECT * FROM student WHERE github<>''";
+	$img_src = "../img/profilepic.png";
+	foreach ($databaseConnection->query($sql) as $row) {
+		$img_src = "";
+		if ($row['profile'] == "") {
+			$img_src = "./img/profilepic.png";
+		} else {
+			$img_src = "../../../MadeinJLM-students/mockup/" . $row['profile'];
+		}
+		echo "<div class='head' id='head_" . $row['ID'] . "' > ";
+		echo "<img class='head_image' id='headimage_" . $row['ID'] . "' src='" . $img_src . "' width='120px' height='110px'>";
+		print_r($row['first_name']);
+		echo "</div>";
+	}
+}
+
 	//filter has instatution
 	if($func=="3"){
 
@@ -392,4 +395,4 @@
     }
 
 ?>
-		
+
