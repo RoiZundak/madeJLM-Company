0cd92452d380d73de855954f4b4c6ec198a39a90
yuval <yuval66ts@gmail.com>
2016-05-30 02:39:04 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ActiveC/V0.1/partials/main.php

--- ActiveC/V0.1/partials/main.php
+++ ActiveC/V0.1/partials/main.php
@@ -41,6 +41,7 @@ if(empty($_SESSION['username'])){
         </div>
         <div class="filters" id="skills">
             Skills
+            
          </div>
          <div class="filters" id="nearby">
             Nearby
