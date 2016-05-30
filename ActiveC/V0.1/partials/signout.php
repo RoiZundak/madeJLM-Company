9f3de57068aec979c590f8a3f5385b198a638ed7
noyBarak <noy.brk@gmail.com>
2016-05-30 19:56:34 +0300

    WORKING : redirecting to login page after logout

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -1,24 +1,4 @@
 <?php
-
-/* session_start();
-
-     unset($_SESSION['username']);
-
-     // Unset all of the session variables.
-     $_SESSION = array();
-
-     //kill the session, also delete the session cookie
-     if (ini_get("session.use_cookies")) {
-         $params = session_get_cookie_params();
-         setcookie(session_name(), '', time() - 42000,
-             $params["path"], $params["domain"],
-             $params["secure"], $params["httponly"]
-         );
-     }
-
-     // Finally, destroy the session.
-     session_destroy();*/
-
 echo
     "<script>
              window.location='http://job.madeinjlm.org/madeJLM-Company/ActiveC/V0.1/#/login';
@@ -27,7 +7,6 @@ echo
              {
                 sessionStorage.setItem('username', 'Not_Valid_User_Name');
                 setTimeout(function(){alert('You have successfully logout.Redirecting to Login page..');},100);
-                //alert('You have successfully logout.Redirecting to Login page..');
              }
      </script>";
 ?>
