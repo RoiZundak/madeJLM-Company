1140adb0d218b30a9aead54b3242331d40b4f84d
noyBarak <noy.brk@gmail.com>
2016-05-30 19:05:14 +0300

    blocking main while user not connected && logout

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -20,12 +20,13 @@
      session_destroy();*/
 echo
     "<script>
-             window.location='http://job.madeinjlm.org/madeJLM-Company/ActiveC/V0.1/#/login';
              //if there is no user connected, skip those lines
              if ( name !== null && name !== 'Not_Valid_User_Name' ) 
              {
                 alert('You have successfully logout.Redirecting to Login page..');
                 sessionStorage.setItem('username', 'Not_Valid_User_Name');
              }
+             window.location='http://job.madeinjlm.org/madeJLM-Company/ActiveC/V0.1/#/login';
+
      </script>";
 ?>
