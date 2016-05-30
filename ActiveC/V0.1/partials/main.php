5042569cdfe4cd6f8024ed61ec421e142bab444e
noyBarak <noy.brk@gmail.com>
2016-05-30 19:03:54 +0300

    blocking main while user not connected && logout

--- ActiveC/V0.1/partials/main.php
+++ ActiveC/V0.1/partials/main.php
@@ -5,8 +5,8 @@ echo "<script>
             var name = sessionStorage.getItem('username');
             if ( name === null || name === 'Not_Valid_User_Name' ) 
             {
-                window.location='http://job.madeinjlm.org/madeJLM-Company/ActiveC/V0.1/#/login';
                 alert('You Must logged in first');
+                window.location='http://job.madeinjlm.org/madeJLM-Company/ActiveC/V0.1/#/login';
             }
             </script>";
 /*No session currently working
