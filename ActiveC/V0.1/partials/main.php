0444f78d07160df8c142a05297b225186fd72c4a
yuval <yuval66ts@gmail.com>
2016-05-30 17:23:28 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ActiveC/V0.1/partials/main.php

--- ActiveC/V0.1/partials/main.php
+++ ActiveC/V0.1/partials/main.php
@@ -155,6 +155,7 @@ if(empty($_SESSION['username'])){
                 xmlhttp.send();
             }
             if(target.id == "skills" ){
+                $("#skill_std").show();
                 xmlhttp.onreadystatechange = function() {
                     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                         document.getElementById("skill_std").innerHTML = xmlhttp.responseText;
