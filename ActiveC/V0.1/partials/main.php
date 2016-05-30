f9425ef2d6acba085cfcdfca4e94525b42604267
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-30 16:48:13 +0300

    login bug smashing

--- ActiveC/V0.1/partials/main.php
+++ ActiveC/V0.1/partials/main.php
@@ -7,6 +7,7 @@ echo "
             if ( name === null || name === 'Not_Valid_User_Name' ) {
                 //document.getElementById(\"re_route_login\").click();
                 document.write('<a href=\'./#/login\' id =\'link_back\' > go back</a>');
+                document.getElementById(\"link_back\").click();
                 //window.stop();
                 <!--
             }
