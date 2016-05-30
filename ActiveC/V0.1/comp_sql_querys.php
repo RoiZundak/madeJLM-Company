9d60877b3328fea014710fb00ac6409c4c8dfffa
Moria Tuchband <moriyatu@gmail.com>
2016-05-30 21:55:12 +0300

    skills filter : datalist

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -288,7 +288,7 @@ function addSkillToList(skill_to_add){
 		echo "
 			<form>	
 			<input type=\"text\" list=\"skills_list\" id='skill_input' class='skills'>
-			<button type=\"button\" oncick='addSkillToList(document.getElementById(\"skill_input\").value)'>+</button>
+			<button type=\"button\" onclick='addSkillToList(document.getElementById(\"skill_input\").value)'>+</button>
 			<datalist id=\"skills_list\">";
 		foreach ($databaseConnection->query($sql) as $row)
 		{
