50ad18987dba5087cb746fb24a29432a25a802f7
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-31 20:27:13 +0300

    skills filter : process form

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -282,7 +282,7 @@
         echo "<script>
         //adds a label and input text containing skill value
         function addSkillToList(skill_to_add){
-            $('#add_skill').after(function() {
+            $('#submit_skills').after(function() {
               return'<label class=\'skills\' for=\'skill_'+skill_to_add+'\'>'+skill_to_add+'</label> <input name=\'skill_'+skill_to_add+'\' type=\'text\' class=\'skills\' style=\'display:none;\' value=\''+ skill_to_add + '\' id=\'skill_'+skill_to_add+'\'>  ' 
             });
         }
@@ -298,7 +298,7 @@
 				echo '<option value='.$row['name'].'>';
 		}
         echo "</datalist>
-                <input type=\"submit\" value=\"Filter\">
+                <input type=\"submit\" value=\"Filter\" id=\"submit_skills\">
                 </form>";
 	}
     if($func=="11"){
