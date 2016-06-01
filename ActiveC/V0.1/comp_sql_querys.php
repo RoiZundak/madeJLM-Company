64a332cce7358e374cdd86fbbf94ed7408e5c546
David Ohayon <DavidOhayonSe@gmail.com>
2016-06-01 08:05:03 +0300

    skills filter : process with Ajax

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -362,10 +362,22 @@ if($func=="9")
               return'<label class=\'skills\' for=\'skill_'+skill_to_add+'\'>'+skill_to_add+'</label><br> <input name=\'skill_'+skill_to_add+'\' type=\'text\' class=\'skills\' style=\'display:none;\' value=\''+ skill_to_add + '\' id=\'skill_'+skill_to_add+'\'>  ' 
             });
         }
+        
+        
+        $( \"#target\" ).submit(function( event ) {
+          var str = $(\"form_skills\").serialize();
+          alert(str);
+          event.preventDefault();
+        });
+
+
+
+        
+        
         </script>";
 
     echo "
-			<form method='post' action='./comp_sql_querys.php?func=10'>	
+			<form method='post' id= 'form_skills'action='./comp_sql_querys.php?func=10'>	
 			<input type=\"text\" list=\"skills_list\" id='skill_input' class='skills'>
 			<input type=\"button\" id = 'add_skill' value = \"add\" class='skills' onclick='addSkillToList(document.getElementById(\"skill_input\").value)'>
 			<br>
