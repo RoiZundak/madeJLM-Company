135fda80419880c644ecfd2225cd3c8506ee3ab2
David Ohayon <DavidOhayonSe@gmail.com>
2016-06-06 21:00:11 +0300

    skills filter

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -385,7 +385,7 @@ if($func=="9")
     echo "
 			<form method='post' id= 'form_skills' action='./comp_sql_querys.php?func=10'>	
 			<input type=\"text\" list=\"skills_list\" id='skill_input' class='skills'>
-			<input type=\"button\" id = 'add_skill' value = \"+\" class='skills' onclick='addSkillToList(document.getElementById(\"skill_input\").value);$(\"#skill_input\").val(\" \");'>
+			<input type=\"button\" id = 'add_skill' value = \"+\" class='skills' onclick='addSkillToList(document.getElementById(\"skill_input\").value);$(\"#skill_input\").val(\"\");'>
 
 			<datalist id=\"skills_list\">";
     foreach ($databaseConnection->query($sql) as $row)
