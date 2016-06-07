78bb78916f159692702fe95d7cee2440f005365c
noyBarak <noy.brk@gmail.com>
2016-06-07 19:42:34 +0300

    working on logout

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -4,7 +4,7 @@
              window.location='http://job.madeinjlm.org/madeJLM-Company/ActiveC/V0.1/#/login';
              var name = sessionStorage.getItem('username');
              //if there is no user connected, skip those lines
-             if ( name !== null && name !== 'Not_Valid_User_Name' ) 
+             if ( name !== 'null' && name !== 'Not_Valid_User_Name' ) 
              {
                 sessionStorage.setItem('username', 'Not_Valid_User_Name');
                 setTimeout(function(){alert('You have successfully logout.Redirecting to Login page..');},100);
