82391df8ef868be3cabf195a968af4bc2b822bf9
noyBarak <noy.brk@gmail.com>
2016-06-07 20:05:17 +0300

    working on filter

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -24,66 +24,62 @@
 		//PDO STYLE :
 		$sql="SELECT * FROM student WHERE ID = '".$q."' LIMIT 1";
 		$img_src = "../img/profilepic.png";
-		foreach ($databaseConnection->query($sql) as $row) {
+		foreach ($databaseConnection->query($sql) as $row)
+		{
 			$img_src ="";
 			if(  $row['profile']=="" ){
 				$img_src = "./img/profilepic.png";
 			}else{
 				$img_src="../../../MadeinJLM-students/mockup/".$row['profile'];
 			}
-			echo "
+			echo "<script>
+				function showMail(obj) 
+				{
+					$('#mailDiv').html(obj);
+				}
+			</script>
+			<table>
+					<tr >
+						<td >
+							<img src=".$img_src." width='120px' height='110px'>
+						</td>
+		
+						<td class ='line_td'>
+							<p>
+								<h2>" . $row['first_name'] ." ". $row['last_name'] . "</h2>"
+						."<br ><div id='mailDiv'><button  type=\"button\"  onclick=\"showMail('". $row['Email'] . "')\">Show Mail</button></div>
+							</p>
+						</td>
+					</tr>
+		
+					<th >
+		
+						<td class ='line_td'><p>
+						<p>
+							<h4>Professional Experince:</h4>
+							2015- Peresnt : NOC operator, Deltathree ,INC.</br>
+							2009-2015:Intel Corp.</br>
+						</td></p>
+					</th>
+					";
+					echo "<table id='show_student_info'>
+							<tr>
+							<th>Firstname</th>
+							<th>Lastname</th>
+							<th>basic education years</th>
+							<th>subject</th>
+							<th>semesters left</th>
+							</tr>";
 
-
-<script>
-function showMail(obj) {
-	$('#mailDiv').html(obj);
-
-}
-</script>
-
-
-	<table>
-            <tr >
-                <td >
-                    <img src=".$img_src." width='120px' height='110px'>
-                </td>
-
-                <td class ='line_td'>
-                    <p>
-                        <h2>" . $row['first_name'] ." ". $row['last_name'] . "</h2>"
-				."<br ><div id='mailDiv'><button  type=\"button\"  onclick=\"showMail('". $row['Email'] . "')\">Show Mail</button></div>
-                    </p>
-                </td>
-            </tr>
-
-            <th >
-
-                <td class ='line_td'><p>
-                <p>
-                    <h4>Professional Experince:</h4>
-                    2015- Peresnt : NOC operator, Deltathree ,INC.</br>
-                    2009-2015:Intel Corp.</br>
-                </td></p>
-            </th>
-            ";
-			echo "<table id='show_student_info'>
-                    <tr>
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>basic education years</th>
-                    <th>subject</th>
-                    <th>semesters left</th>
-                    </tr>";
-
-			echo "<tr>";
-			echo "<td>" . $row['first_name'] . "</td>";
-			echo "<td>" . $row['last_name'] . "</td>";
-			echo "<td>" . $row['basic_education_years'] . "</td>";
-			echo "<td>" . $row['basic_education_subject'] . "</td>";
-			echo "<td>" . $row['semesters_left'] . "</td>";
-			echo "</tr>";
-			echo "</table>";
-		}
+					echo "<tr>";
+					echo "<td>" . $row['first_name'] . "</td>";
+					echo "<td>" . $row['last_name'] . "</td>";
+					echo "<td>" . $row['basic_education_years'] . "</td>";
+					echo "<td>" . $row['basic_education_subject'] . "</td>";
+					echo "<td>" . $row['semesters_left'] . "</td>";
+					echo "</tr>";
+					echo "</table>";
+			}
 
 
 
