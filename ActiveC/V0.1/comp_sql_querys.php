b0bea35389888ac860de7c61dca587c419de4609
yuval <yuval66ts@gmail.com>
2016-06-08 00:42:06 +0300

    Function to show mail

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -1,3 +1,13 @@
+<script>
+	function myFunc(email)
+	{
+		$('#mailDiv').html(email);
+		xmlhttp.open('GET', 'comp_sql_querys.php?q=\" ".$q." \"&func=\"11\", true);
+		xmlhttp.send();
+	}
+
+</script>
+
 <?php
 	require_once "php/db_connect.php";
 	$databaseConnection =connect_to_db();
@@ -25,15 +35,6 @@
 			}else{
 				$img_src="../../../MadeinJLM-students/mockup/".$row['profile'];
 			}
-			echo "<script>
-								     function myFunc(email)
-								    	{
-											$('#mailDiv').html(email);
-											xmlhttp.open('GET', 'comp_sql_querys.php?q=\" ".$q." \"&func=\"11\", true);
-											xmlhttp.send();
-								    	}
-								
-								</script>";
 			echo "<table>
 					<tr >
 						<td >
@@ -44,7 +45,7 @@
 							<p>
 								<h2>" . $row['first_name'] ." ". $row['last_name'] . "</h2><br >
 							<div id='mailDiv'>
-								
+							
 								
 								<button type=\"button\" id = \"but\" onclick=\"myFunc('hhhhh');\"> ShowMail </button>
 					
