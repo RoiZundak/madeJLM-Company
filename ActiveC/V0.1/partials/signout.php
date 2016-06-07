d5b90af5c176905e35e93d460a55f8cb50db4d0c
noyBarak <noy.brk@gmail.com>
2016-06-07 19:36:11 +0300

    working on logout

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -1,6 +1,6 @@
 <?php
-echo
-"<script>
+    echo
+    "<script>
              window.location='http://job.madeinjlm.org/madeJLM-Company/ActiveC/V0.1/#/login';
              //if there is no user connected, skip those lines
              if ( name !== null && name !== 'Not_Valid_User_Name' ) 
