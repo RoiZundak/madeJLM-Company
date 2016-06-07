29902ea9fb1b5ef684266c2594b576646c4b671a
Roi <roizundak@Gmail.com>
2016-06-07 19:27:53 +0300

    RED:compsql file

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -40,7 +40,7 @@
                 <td class ='line_td'>
                     <p>
                         <h2>" . $row['first_name'] ." ". $row['last_name'] . "</h2>"
-				."<br >" .$row['Email'] . "
+				."<br ><button type=\"button\">Click Me!</button>"."
                     </p>
                 </td>
             </tr>
