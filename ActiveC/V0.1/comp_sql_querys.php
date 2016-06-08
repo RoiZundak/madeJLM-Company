978dd468e0adad3f0997849a3d02481d44f348a6
noyBarak <noy.brk@gmail.com>
2016-06-08 16:00:59 +0300

    build sql querys for students info

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -59,7 +59,7 @@
 				array_push($list_skills,$skill['skill_id']);
 			}
 			$skills_name ="SELECT name FROM skills WHERE id IN (".implode(',',$list_skills).")";
-			if ($skills_name !== "")
+			if (count($list_skills)>0)
 			{
 				foreach ($databaseConnection->query($skills_name) as $skill)
 				{
