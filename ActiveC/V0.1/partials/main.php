3bf13b4c2288f356a96317657f198f593767ad35
noyBarak <noy.brk@gmail.com>
2016-06-08 17:52:44 +0300

    edit students info - order by strength

--- ActiveC/V0.1/partials/main.php
+++ ActiveC/V0.1/partials/main.php
@@ -29,7 +29,7 @@
             require_once "../php/db_connect.php";
             $databaseConnection =connect_to_db();
             //get all students
-            $sql = 'SELECT * FROM student WHERE Activated=1';
+            $sql = 'SELECT * FROM student WHERE Activated=1 ORDER BY profile_strength';
             $img_src = "../img/profilepic.png";
             foreach ($databaseConnection->query($sql) as $row)
             {
