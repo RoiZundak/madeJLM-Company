41330d5eba60110d1dc211d0badad5bb70d3ab9d
noyBarak <noy.brk@gmail.com>
2016-06-08 18:47:23 +0300

    edit students info - personal information

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -78,9 +78,9 @@
 			}
 
 			$phone_number="";
-			if ($row['phone_number'] !== "")
+			if ($row['phone_number'] !== "0" )
 				$phone_number=$row['phone_number'];
-			
+
 			$sentence="Studies for a ".$degree_name." in ".$row['basic_education_subject']." at ".$college_name." with GPA of ".$row['grade_average']." and has ".$row['semesters_left']." semesters left.";
 			$job_per=$row['first_name']." is avaliable for ";
 			switch($row['job_percent'])
