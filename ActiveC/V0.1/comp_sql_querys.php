82682e2634a89659ffb3995c0ba182ae8bf16f59
noyBarak <noy.brk@gmail.com>
2016-06-08 14:16:46 +0300

    Editing Student info

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -56,6 +56,7 @@
 						".$link_string."
 						<!--cv-->
                     </td>    
+                    <br>
                     <td>
 							<div id='mailDiv'>
 								<button id = 'std_mail_".$row['ID']."' class='filters' onclick='$(\"#mailDiv\").html(".$maito_string.");' >Show Mail </button>			
