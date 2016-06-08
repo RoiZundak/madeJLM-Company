40149b5916babe55b496dbcd0c0588807176576a
Roi <roizundak@Gmail.com>
2016-06-09 01:45:23 +0300

    RED: Start to work on show student

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -148,7 +148,7 @@
 				</tr>
 				
 				<!--Second Line: Phone + Mail-->
-				<tr>
+				<tr class=\"border_bottom\">
 						<td id='phoneDiv'>
 						    <button id = 'std_phone_" . $row['ID'] . "' class='filters' onclick='$(\"#phoneDiv\").html(\"" . $phone_number . "\");' >
 								Show Phone
@@ -164,14 +164,14 @@
                 
                 	<!--Third Line: Sentence-->
                 	
-                 <tr >
+                 <tr class=\"border_bottom\">
                 	<td>
                 		" . $sentence . "
 					</td>
                 </tr>
                 
                 <!--Four Line: JobPer + CurrJob-->
-                 <tr>
+                 <tr class=\"border_bottom\">
                 	<td>
                 		" . $job_per . "
 					</td>
@@ -182,13 +182,13 @@
                 </tr>
                 
                 <!--Fifth Line: All Skills + ShowAll-->
-                <tr>
+                <tr class=\"border_bottom\">
                 	<td>
                 		<h4><b>".$show_all_skills."</b></h4> ".$all_skills."
 					</td>
                 </tr>
                 <!--Six Line: Sum + Experince-->
-                <tr>
+                <tr class=\"border_bottom\">
                 	<td>
 							<h4><b>" . $sum . "</h4></b>" . $summary . "
 					</td>
