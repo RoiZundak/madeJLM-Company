6b00a8fd3afc2c0efcd8d6448124277234879f7a
yuval <yuval66ts@gmail.com>
2016-06-08 21:15:04 +0300

    block user after 5 attempts

--- ActiveC/V0.1/partials/login.php
+++ ActiveC/V0.1/partials/login.php
@@ -37,7 +37,9 @@
                 if($newTime < $results['block'])
                 {
                     $errMsg .= 'Time block<br>';
-
+                    $sql_update = "UPDATE company SET block = null WHERE username = '" . $username . "'";
+                    $update = $databaseConnection ->prepare($sql_update);
+                    $update->execute();
                     echo("<a id='re_route_login' href ='../#/login'></a>
                     <script>
                         alert('Your block time did not over yet.');
@@ -98,7 +100,7 @@
 
                     $d=strtotime("+15 minutes");
 
-                    $sql_update="UPDATE company SET block = NOW() WHERE username = '".$username."'";
+                    $sql_update="UPDATE company SET block = date(\"Y-m-d h:i:sa\") WHERE username = '".$username."'";
                     $update = $databaseConnection ->prepare($sql_update);
                     $update->execute();
                 }
