1b670745258c6505529098ead4b05cdb978fc5e6
Roi <roizundak@Gmail.com>
2016-06-08 23:57:58 +0300

    RED: Start to work on show student

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -135,9 +135,11 @@
 			    <tr>
 			        <td  width=\"100%\">
 			            <img class='head_image' src =" . $img_src . " width ='120px' height='110px'>
+			            <div id='bubble'>
 			            " . $git_string . "
 						" . $link_string . "
 						" . $cv_file . "
+						</div>
 			            <h2>" . $row['first_name'] . " " . $row['last_name'] . "</h2>
 			            
                     </td>  
