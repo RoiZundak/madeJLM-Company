93c0d02ce564b9c7d48169c1cb608d3a6b10d7a3
Roi <roizundak@Gmail.com>
2016-06-09 04:56:13 +0300

    RED:Icons(Linkedin)

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -36,8 +36,8 @@ $databaseConnection->query($sql);
 			$link_string = "";
 			if ($row['linkedin'] !== "") {
 				$link_string = "
-								<a href=\"" . $row['linkedin'] . "\">
-								<a href=\"https://il.linkedin.com/pub/roi-cohen/96/643/99a\" style=\"text-decoration:none;\"><span style=\"font: 80% Arial,sans-serif; color:#0783B6;\"><img src=\"http://s.c.lnkd.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png\" width=\"20\" height=\"15\" alt=\"View Roi Cohen's LinkedIn profile\" style=\"vertical-align:middle;\" border=\"0\">&nbsp;View Roi Cohen's profile</span></a>
+								
+								<a href=\"" . $row['linkedin'] . "\" style=\"text-decoration:none;\"><span style=\"font: 80% Arial,sans-serif; color:#0783B6;\"><img src=\"http://s.c.lnkd.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png\" width=\"20\" height=\"15\" alt=\"View Roi Cohen's LinkedIn profile\" style=\"vertical-align:middle;\" border=\"0\">&nbsp;View Roi Cohen's profile</span></a>
 								</a>
  								";
 			}
