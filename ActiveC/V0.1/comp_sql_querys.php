181ad01030d65e8e612db119dae1cc1600b601f4
noyBarak <noy.brk@gmail.com>
2016-06-09 20:10:25 +0300

    working on adding phone pic

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -152,9 +152,9 @@ if($func=="1")
 
 		$phone_pic="";
 		if($row['phone_number']!=="") {
-			$phone_number='0'.$row['phone_number'];
+			$phone_number="0".$row['phone_number'];
 			$phone_pic = "<div> 
-				<img src=\"./img/telephoneIcon.jpg\" width='35' height='35' onclick='$(\"#phoneDiv\").html(" . $phone_number . ");'/>
+				<img src=\"./img/telephoneIcon.jpg\" width='35' height='35' onclick='$(\"#phoneDiv\").html('0'.".$phone_number.");'/>
 				</div>";
 		}
 
