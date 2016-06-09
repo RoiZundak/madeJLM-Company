d469bbac42e35ffe9f5522c0700c31bb7931032f
noyBarak <noy.brk@gmail.com>
2016-06-09 22:30:15 +0300

    editing table information

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -180,9 +180,8 @@ if($func=="1")
 				            " .$git_string. "  " .$link_string. "   " .$cv_file. " 
 						</div>
 						<div>
-							".$mail_pic."
+							".$mail_pic.$phone_pic."
 						</div>
-						<div> ".$phone_pic." </div>
                     </td>
                       
 				</tr>
