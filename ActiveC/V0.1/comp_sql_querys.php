8878269f21cd51239a6a3ba32b0c5c354f0dbaf6
noyBarak <noy.brk@gmail.com>
2016-06-09 17:54:42 +0300

    editing skills func

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -440,7 +440,8 @@ if($func=="1")
 				$temp_array=array($skill,$value); //create new array that contains time && skills
                 print_r($temp_array);
 				$len = count($skills_arr)-1;
-				array_push($skills_arr[$len],$temp_array);
+				for ($i=1;$i<$len;$i++)
+					array_push($skills_arr[$i],$temp_array);
             }
 
 		}
