84406a8467d4fa190e1b64d8c0e5b2acc2118de9
David Ohayon <DavidOhayonSe@gmail.com>
2016-06-10 18:39:43 +0300

    Skills with years

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -488,7 +488,7 @@ if($func=="1")
                 echo "first : ";
                 $len = count($id);
                 for ($j=0;$j<$len;$j++){
-                    $std_id[$j]=$id[$i];
+                    $std_id[$j]=$id[$i][0];
                     echo $std_id[$j].", ";
                 }
                 echo "<br>";
