f4dac8669dad463fc88170105b36319787d6a581
noyBarak <noy.brk@gmail.com>
2016-06-11 14:58:32 +0300

    timout alert on signout

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -8,7 +8,7 @@
 <?php
     echo
     "<script>
-             window.location='http://job.madeinjlm.org/madeJLM-Company/ActiveC/V0.1/#/login';
+             window.location='#/login';
              var name = sessionStorage.getItem('username');
              //if there is no user connected, skip those lines
              if ( name !== 'null' && name !== 'Not_Valid_User_Name' ) 
