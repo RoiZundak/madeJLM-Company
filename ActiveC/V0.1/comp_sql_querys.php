5f06d404fd05fb5c13a9a9e2c30a60d017396c99
David Ohayon <DavidOhayonSe@gmail.com>
2016-06-13 11:32:53 +0300

    Bulk fetch students in Skills in filter- try with block size 200

--- ActiveC/V0.1/comp_sql_querys.php
+++ ActiveC/V0.1/comp_sql_querys.php
@@ -560,6 +560,36 @@
 			print_r($row['first_name']);
 			echo "</div>";
 		}
+
+
+        $temp=0;
+        //$sql = 'SELECT * FROM student WHERE Activated=1 ORDER BY profile_strength DESC '; WORKING QUERY
+        while($temp<1){
+            $sql = "SELECT * FROM student WHERE ID IN(".implode(',',$std_id).") ORDER BY profile_strength DESC LIMIT ".$bulk_size." OFFSET ".($temp*$bulk_size);
+
+            $img_src = "../img/profilepic.png";
+            $count_recived=0;
+            foreach ($databaseConnection->query($sql) as $row)
+            {
+                if(  $row['profile']=="" )
+                    $img_src = "../V0.1/img/profilepic.png";
+                else
+                    $img_src="../../../MadeinJLM-students/mockup/".$row['profile'];
+                echo "<div class='head' id='head_".$row['ID']."' > ";
+                echo "<img class='head_image' id='headimage_".$row['ID']. "' src='".$img_src."' width='120px' height='110px'>";
+                print_r($row['first_name']);
+                echo "</div>";
+                $count_recived++;
+            }
+            if($count_recived != $bulk_size){
+                break;
+            }
+            $temp++;
+        }
+        
+
+
+
 	}
 
     //increment student contact stats
