179dd80572f6fafeb2b71aabb061c3927374d51c
noyBarak <noy.brk@gmail.com>
2016-06-15 21:34:16 +0300

    signout.php - change all links to absolute path

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -8,13 +8,13 @@
 <?php
     echo
     "<script>
-             window.location='http://job.madeinjlm.org/#/login';
+             window.location='#/login';
              var name = sessionStorage.getItem('username');
              //if there is no user connected, skip those lines
              if ( name !== 'null' && name !== 'Not_Valid_User_Name' ) 
              {
                 sessionStorage.setItem('username', 'Not_Valid_User_Name');
-                setTimeout(function(){alert('You have successfully logout.Redirecting to Login page..');},150);
+                setTimeout(function(){alert('You have successfully logout. Redirecting to Login page..');},150);
              }
      </script>";
 ?>
