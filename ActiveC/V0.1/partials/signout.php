82e819094377cf9a2660b12ff99b1056c3cb94a8
David Ohayon <DavidOhayonSe@gmail.com>
2016-06-18 20:17:15 +0300

    logout admin

--- ActiveC/V0.1/partials/signout.php
+++ ActiveC/V0.1/partials/signout.php
@@ -10,10 +10,12 @@
     "<script>
              window.location='#/login';
              var name = sessionStorage.getItem('username');
+             var AdminName = sessionStorage.getItem('username_Admin');
              //if there is no user connected, skip those lines
-             if ( name !== 'null' && name !== 'Not_Valid_User_Name' ) 
+             if ( (name !== 'null' && name !== 'Not_Valid_User_Name') || (AdminName !== 'null' && AdminName !== 'Not_Valid_User_Name') ) 
              {
                 sessionStorage.setItem('username', 'Not_Valid_User_Name');
+                sessionStorage.setItem('username_Admin', 'Not_Valid_User_Name');
                 setTimeout(function(){alert('You have successfully logout. Redirecting to Login page..');},150);
              }
      </script>";
