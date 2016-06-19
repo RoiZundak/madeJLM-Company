07cafac952a0dc5d24619fe87847dd02c4843da7
noyBarak <noy.brk@gmail.com>
2016-06-19 22:39:16 +0300

    retrive reset_password.php to his last working version

--- ActiveC/V0.1/partials/reset_password.php
+++ ActiveC/V0.1/partials/reset_password.php
@@ -6,38 +6,37 @@
 ********************************************************************************
 *****************************************************************************-->
 <?php
-    //connect to db
-    require_once "../php/db_connect.php";
-    $databaseConnection =connect_to_db();
-    if($_GET['t']=="company"){
-        $table = "company";
-    }else{
-        $table = "admin";
-    }
-    if($table === "company"){
-        $sql = "SELECT * FROM company WHERE email = :email";
-    }else{
-        $sql = "SELECT * FROM admin WHERE Email = :email";
-    }
-    $stmt = $databaseConnection->prepare($sql);
-    $stmt->bindParam(":email",$_GET['mail']);
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-    foreach($result as $row){
-        $exp = $row['f_exp'];
-        $code = $row ['f_pass'];
-    }
-
-    if($exp !=$_GET['e'] || $exp <= time() || strcmp ($code , $_GET['p'])!=0 )
-    {
-        //first condition : someone is trying something fishy.
-        //second : expiration date has already passed
-        //third : code is not the same in db and link
-        echo "<script>
+//connect to db
+require_once "../php/db_connect.php";
+$databaseConnection =connect_to_db();
+if($_GET['t']=="company"){
+    $table = "company";
+}else{
+    $table = "admin";
+}
+if($table === "company"){
+    $sql = "SELECT * FROM company WHERE email = :email";
+}else{
+    $sql = "SELECT * FROM admin WHERE Email = :email";
+}
+$stmt = $databaseConnection->prepare($sql);
+$stmt->bindParam(":email",$_GET['mail']);
+$stmt->execute();
+$result = $stmt->fetchAll();
+foreach($result as $row){
+    $exp = $row['f_exp'];
+    $code = $row ['f_pass'];
+}
+if($exp !=$_GET['e'] || $exp <= time() || strcmp ($code , $_GET['p'])!=0 )
+{
+    //first condition : someone is trying something fishy.
+    //second : expiration date has already passed
+    //third : code is not the same in db and link
+    echo "<script>
 			window.location='#/login';
             setTimeout(function(){alert('This link has expired.Redirecting to login page..');},100);</script>";
-        exit;
-    }
+    exit;
+}
 ?>
 
 
@@ -62,8 +61,8 @@
                         <input type="password" name="new_pass_conf" id="new_pass_conf">
                     </p>
                     <?php
-                        echo "<input type ='hidden' name='e_mail' value='".$_GET['mail']."'>";
-                        echo "<input type ='hidden' name='type_b' value='".$table."'>";
+                    echo "<input type ='hidden' name='e_mail' value='".$_GET['mail']."'>";
+                    echo "<input type ='hidden' name='type_b' value='".$table."'>";
                     ?>
 
                     <button type="submit" name = "submit" class="login-button">Login</button>
