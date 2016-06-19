645b0e90198def1c879b9de82c3d8ae2a0b07fa0
Moria Tuchband <moriyatu@gmail.com>
2016-06-19 17:13:44 +0300

    clean up code

--- ActiveC/V0.1/php/db_connect.php
+++ ActiveC/V0.1/php/db_connect.php
@@ -13,6 +13,7 @@ function connect_to_db(){
     define('_DB_PASSWORD', 'q1w2e3r4');
     define('_DATABASE_NAME_', 'jobmadei_db');
         //PDO Database Connection
+    
     try {
         $databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
         $databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
